50db8f6d1e3768d2df010447892ac22b1ed2d8d5
Andris <laczkovicsandris07.education@gmail.com>
2026-06-11 23:56:27 +0200

    Bro Code Python - 68. images.py

---
+++ Bro Code - Python Full Course/gui/68. images.py
@@ -0,0 +1,33 @@
+# PyQt5 introduction
+
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
+from PyQt5.QtGui import QPixmap
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("My cool first GUI")
+        self.setGeometry(1030, 470, 500, 500)
+        
+        label = QLabel(self)
+        label.setGeometry(0, 0, 250, 250)
+
+        pixmap = QPixmap("gui\images\profile_pic.jpg")
+        label.setPixmap(pixmap)
+
+        label.setScaledContents(True)
+
+        label.setGeometry((self.width() - label.width()) // 2,
+                          (self.height() - label.height()) // 2,
+                          label.width(),
+                          label.height())
+
+def main():
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
