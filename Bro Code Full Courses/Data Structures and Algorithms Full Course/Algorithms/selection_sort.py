1ae11bfbd12e606910032a705cbc9846bfc4e5b1
Andris <laczkovicsandris07.education@gmail.com>
2026-07-12 18:31:33 +0200

    Bro Code D&A - 13. selection sort.md python variable name change

--- Bro Code Full Courses/Data Structures and Algorithms Full Course/Algorithms/selection_sort.py
+++ Bro Code Full Courses/Data Structures and Algorithms Full Course/Algorithms/selection_sort.py
@@ -1,13 +1,13 @@
 # Algorithm
 def selection_sort(array):
     for i in range(len(array)):
-        min = i
+        min_index = i
         for j in range(i+1, len(array)):
-            if array[min] > array[j]:
-                min = j
+            if array[min_index] > array[j]:
+                min_index = j
         temp = array[i]
-        array[i] = array[min]
-        array[min] = temp
+        array[i] = array[min_index]
+        array[min_index] = temp
 
 array = [8, 7, 9, 2, 3, 1, 5, 4, 6]
 separator = ";"
